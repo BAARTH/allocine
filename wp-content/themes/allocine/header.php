5c626779a22f1f80e2513df7eba517b87c046b78
BAARTH <balossini.a@hotmail.fr>
2017-10-20 12:39:22 +0200

    Widget - Menus and other

--- wp-content/themes/allocine/header.php
+++ wp-content/themes/allocine/header.php
@@ -22,4 +22,14 @@
                     <h2><?php bloginfo("description"); ?></h1>
                 </div>
             </div>
+            <?php // SYNTAXE : wp_nav_menu( array $args = array() )
+            $args=array(
+                'theme_location' => 'header', // nom du slug
+                'menu' => 'header_fr', // nom à donner cette occurence du menu
+                'menu_class' => 'menu_header', // class à attribuer au menu
+                'menu_id' => 'menu_id' // id à attribuer au menu
+                // voir les autres arguments possibles sur le codex
+            );
+            wp_nav_menu($args);
+            ?>
         </header>
